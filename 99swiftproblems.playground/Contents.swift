2b2c998b97f0cbc11effc7919c2f463546abbe03
Andre Rodrigues <andre.msr@icloud.com>
2018-10-19 01:31:13 -0300

    Finish 10 and 11, Improve how exercises are printed

--- 99swiftproblems.playground/Contents.swift
+++ 99swiftproblems.playground/Contents.swift
@@ -1,9 +1,9 @@
-func exercise(_ no: Int, _ description: String? = nil, closure: () -> Void) {
+func exercise(_ no: Int, _ description: String? = nil, closure: () -> CustomStringConvertible?) {
     print("----- Exercise Nº\(no) -----")
     if let description = description {
         print("\(description):")
     }
-    closure()
+    print(closure() ?? "nil")
     print()
 }
 
@@ -58,7 +58,7 @@ extension List {
 }
 
 exercise(1, "last") {
-    print(List(1, 1, 2, 3, 5, 8).last)
+    return List(1, 1, 2, 3, 5, 8).last
 }
 
 extension List {
@@ -72,9 +72,11 @@ extension List {
 }
 
 exercise(2, "penultimate") {
-    print(List(1, 1, 2, 3, 5, 8).pennultimate)
-    print(List(1, 2).pennultimate)
-    print(List(1).pennultimate)
+    return """
+    \(List(1, 1, 2, 3, 5, 8).pennultimate)
+    \(List(1, 2).pennultimate)
+    \(List(1).pennultimate)
+    """
 }
 
 extension List {
@@ -85,7 +87,7 @@ extension List {
 
 exercise(3, "subscript") {
     let list = List(1, 1, 2, 3, 5, 8)
-    print(list[2])
+    return list[2]
 }
 
 extension List {
@@ -99,7 +101,7 @@ extension List {
 }
 
 exercise(4, "length") {
-    print(List(1, 1, 2, 3, 5, 8).length)
+    return List(1, 1, 2, 3, 5, 8).length
 }
 
 extension List {
@@ -138,8 +140,10 @@ extension List {
 exercise(5, "reverse") {
     let listToReverse = List(1, 1, 2, 3, 5, 8)
     listToReverse.reverse()
-    print(listToReverse)
-    print(List(1, 1, 2, 3, 5, 8).reversed())
+    return """
+    \(listToReverse)
+    \(List(1, 1, 2, 3, 5, 8).reversed())
+    """
 }
 
 extension List where T:Equatable {
@@ -160,7 +164,7 @@ extension List where T:Equatable {
 }
 
 exercise(6, "isPalindrome") {
-    print(List(2, 1, 2, 3, 2, 1, 2).isPalindrome())
+    return List(2, 1, 2, 3, 2, 1, 2).isPalindrome()
 }
 
 extension List {
@@ -188,7 +192,7 @@ extension List {
 }
 
 exercise(7, "flatten") {
-    print(List<Any>(List<Any>(1,
+    return List<Any>(List<Any>(1,
                               1),
                     2,
                     List<Any>(3,
@@ -197,7 +201,7 @@ exercise(7, "flatten") {
                                         List<Any>(13,
                                                   List<Any>(21,
                                                             34)))))
-        .flatten())
+        .flatten()
 
 }
 
@@ -218,7 +222,7 @@ extension List where T: Equatable {
 exercise(8, "compress") {
     let compressedList = List("a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e")
     compressedList.compress()
-    print(compressedList)
+    return compressedList
 }
 
 extension List where T: Equatable {
@@ -242,7 +246,7 @@ extension List where T: Equatable {
 
 exercise(9, "pack") {
     let packedList = List("a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e")
-    print(packedList.pack())
+    return packedList.pack()
 }
 
 extension List where T: Equatable {
@@ -262,16 +266,53 @@ extension List where T: Equatable {
 
 exercise(10, "encode") {
     let list = List("a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e")
-    print(list.encode())
+    return list.encode()
 }
 
 extension List where T: Equatable {
     func encodeModified() -> List<Any> {
-        let encoded = self.encoded()
+        let l = pack()
+        var v = [Any]()
+        var current = Optional.some(l)
+
+        while current != nil, let unwrappedCurrent = current?.value {
+            let length = unwrappedCurrent.length
+            let value: Any = length == 1
+                ? unwrappedCurrent.value
+                : (unwrappedCurrent.length, unwrappedCurrent.value)
+            v.append(value)
+            current = current?.nextItem
+        }
+
+        return List<Any>(v)!
     }
 }
 
 exercise(11, "encodeModified") {
     let list = List("a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e")
-    list.encodeModified()
+    return list.encodeModified()
+}
+
+extension List where T == (Int, String) {
+    private func disjointed() -> (List<String>, List<String>) {
+        var head = List<String>(value.1)
+        var tail = head
+        for i in 1..<value.0 {
+            let nextItem = List<String>(value.1)
+            tail.nextItem = nextItem
+            tail = nextItem
+        }
+        return (head, tail)
+    }
+
+    func decode() -> List<String> {
+        let (head, tail) = disjointed()
+        tail.nextItem = nextItem?.decode()
+        return head
+    }
+}
+
+exercise(12, "decode") {
+    let list = List((4, "a"), (1, "b"), (2, "c"), (2, "a"), (1, "d"), (4, "e"))
+    return list.decode()
 }
